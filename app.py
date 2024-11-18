96729daedf08db33d1d0268a5afc7dd6a592025b
netas <netasher360@gmail.com>
2024-11-18 18:52:28 +0200

    added a seperate file for db models

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager, login_user,logout_user, current_user, login_required
+from flask_login import LoginManager, login_user,logout_user, current_user, login_required
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from models import Company, Job, Comment, Location, Users, Industry
 
 app = Flask(__name__, template_folder='templates')
 
@@ -26,6 +27,8 @@ bcrypt = Bcrypt(app)
 
 
 
+
+############### APP DB MANAGEMENT ###############
 #daniel local connection:
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@localhost:{port}/{databasename}".format(
     username="root",
@@ -59,66 +62,71 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
-class Comment(db.Model):
-    __tablename__ = "comments"
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
+#########################################
 
-class Company(db.Model):
-    __tablename__ = 'companies'
-    company_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    description = db.Column(db.Text)
-    website = db.Column(db.String(255))
-    logo_url = db.Column(db.String(255))
-    jobs = db.relationship('Job', back_populates='company', lazy=True)
-    locations = db.relationship('Location',
-                                secondary='company_locations',
-                                backref=db.backref('companies', lazy=True))
-    industries = db.relationship('Industry',
-                                 secondary='company_industries',
-                                 backref=db.backref('companies', lazy=True))
+############### DB MODELS ###############
+# class Comment(db.Model):
+#     __tablename__ = "comments"
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(4096))
+#
+# class Company(db.Model):
+#     __tablename__ = 'companies'
+#     company_id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     description = db.Column(db.Text)
+#     website = db.Column(db.String(255))
+#     logo_url = db.Column(db.String(255))
+#     jobs = db.relationship('Job', back_populates='company', lazy=True)
+#     locations = db.relationship('Location',
+#                                 secondary='company_locations',
+#                                 backref=db.backref('companies', lazy=True))
+#     industries = db.relationship('Industry',
+#                                  secondary='company_industries',
+#                                  backref=db.backref('companies', lazy=True))
+#
+# class Job(db.Model):
+#     __tablename__ = 'jobs'
+#     job_id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String, nullable=False)
+#     description = db.Column(db.String, nullable=False)
+#     company_id = db.Column(db.Integer, db.ForeignKey('companies.company_id'), nullable=False)
+#     company = db.relationship('Company', back_populates='jobs')
+#
+# class Location(db.Model):
+#     __tablename__ = 'locations'
+#     location_id = db.Column(db.Integer, primary_key=True)
+#     city = db.Column(db.String(100))
+#     state = db.Column(db.String(100))
+#     country = db.Column(db.String(100), nullable=False)
+#
+# class Industry(db.Model):
+#     __tablename__ = 'industries'
+#     industry_id = db.Column(db.Integer, primary_key=True)
+#     industry_name = db.Column(db.String(100), nullable=False)
+#
+# class Users(db.Model, UserMixin):
+#     __tablename__ = 'users'
+#     uid = db.Column(db.Integer, primary_key=True)
+#     user_name = db.Column(db.String(80), nullable=False, unique=True)
+#     password = db.Column(db.String(80), nullable=False)
+#     email = db.Column(db.String(80), nullable=False)
+#
+#     def get_id(self):
+#         return self.uid
+#
+#
+# company_locations = db.Table('company_locations',
+#     db.Column('company_id', db.Integer, db.ForeignKey('companies.company_id'), primary_key=True),
+#     db.Column('location_id', db.Integer, db.ForeignKey('locations.location_id'), primary_key=True)
+# )
+#
+# company_industries = db.Table('company_industries',
+#     db.Column('company_id', db.Integer, db.ForeignKey('companies.company_id'), primary_key=True),
+#     db.Column('industry_id', db.Integer, db.ForeignKey('industries.industry_id'), primary_key=True)
+# )
 
-class Job(db.Model):
-    __tablename__ = 'jobs'
-    job_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    company_id = db.Column(db.Integer, db.ForeignKey('companies.company_id'), nullable=False)
-    company = db.relationship('Company', back_populates='jobs')
-
-class Location(db.Model):
-    __tablename__ = 'locations'
-    location_id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(100))
-    state = db.Column(db.String(100))
-    country = db.Column(db.String(100), nullable=False)
-
-class Industry(db.Model):
-    __tablename__ = 'industries'
-    industry_id = db.Column(db.Integer, primary_key=True)
-    industry_name = db.Column(db.String(100), nullable=False)
-
-class Users(db.Model, UserMixin):
-    __tablename__ = 'users'
-    uid = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), nullable=False)
-
-    def get_id(self):
-        return self.uid
-
-
-company_locations = db.Table('company_locations',
-    db.Column('company_id', db.Integer, db.ForeignKey('companies.company_id'), primary_key=True),
-    db.Column('location_id', db.Integer, db.ForeignKey('locations.location_id'), primary_key=True)
-)
-
-company_industries = db.Table('company_industries',
-    db.Column('company_id', db.Integer, db.ForeignKey('companies.company_id'), primary_key=True),
-    db.Column('industry_id', db.Integer, db.ForeignKey('industries.industry_id'), primary_key=True)
-)
+###########################################################################
 @app.route('/')
 def index():
     return render_template('index2.html')
@@ -150,6 +158,7 @@ def company_detail(company_id):
                          industries=company.industries)
 
 @app.route("/comments_page", methods=["GET", "POST"])
+@login_required
 def comments_page():
     if request.method == "GET":
         return render_template("comments_page.html", comments=Comment.query.all())
@@ -183,7 +192,7 @@ def sign_up_page():
         db.session.add(user)
         db.session.commit()
 
-        return redirect('/test',current_user=current_user.user_name)
+        return redirect('/',current_user=current_user.user_name)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -204,7 +213,7 @@ def login():
             next_page = request.args.get('next')
 
             # Redirect the user to the next page or to the default page ('/test')
-            return redirect(next_page or '/')
+            return redirect('/')
         else:
             return redirect('/betterLog')
 
